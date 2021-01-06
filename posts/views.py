e81125a3e3768ec97985c50ef46050c042613a21
brahim024 <ifninos168@gmail.com>
2021-01-06 15:21:14 +0100

    add docker files and compose

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
-import json
+
 # Create your views here.
-def data(request):
-	url='http://newsapi.org/v2/everything?q=bitcoin&from=2020-12-05&sortBy=publishedAt&apiKey=33a794feb0c24d07810406fa7dc3e978'
-	data=requests.get(url).json()
-	return HttpResponse(data)
+def get_news(request):
+	url='http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=33a794feb0c24d07810406fa7dc3e978'
+	data=requests.get(url)
+	d=data.json()
+
+	
+	return render(request,'home.html',{'data':d})
