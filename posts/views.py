53fa68f071d5a7ac8df81f223043a8148d8d353a
brahim024 <ifninos168@gmail.com>
2021-01-07 14:14:02 +0100

    organic data from api url  in views

--- posts/views.py
+++ posts/views.py
@@ -4,9 +4,18 @@ import requests
 
 # Create your views here.
 def get_news(request):
-	url='http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=33a794feb0c24d07810406fa7dc3e978'
-	data=requests.get(url)
-	d=data.json()
-
+	url='http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=33a794feb0c24d07810406fa7dc3e978'
+	r=requests.get(url).json()
 	
-	return render(request,'home.html',{'data':d})
+
+	context_data={
+	'author':r['articles'][0]['author'],
+	'title':r['articles'][1]['title'],
+	'description':r['articles'][2]['description'],
+	'url':r['articles'][3]['url'],
+	'urlToImage':r['articles'][4]['urlToImage'],
+	'publishedAt':r['articles'][5]['publishedAt'],
+	
+	}
+	print(context_data)
+	return render(request,'home.html',{'data':context_data})
